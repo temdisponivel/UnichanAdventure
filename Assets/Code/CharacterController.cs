2ed2a98e89795c1aba315228cff221177285c7ef
unknown <matheus.aacandido1@CAS419596W0705.senacsp.edu.br>
2016-03-22 22:06:04 -0300

    First commit;

---
+++ Assets/Code/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public Animator _animator = null;
+	public Rigidbody _rigid = null;
+
+	void Update () {
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+		_animator.SetFloat ("Walking", vertical);
+		_animator.SetFloat ("Velocity", this._rigid.velocity.magnitude);
+		this.transform.Rotate(Vector3.up, horizontal * Time.deltaTime * 20 / (this._rigid.velocity.magnitude + .1f));
+	}
+
+	void FixedUpdate() {
+		float vertical = Input.GetAxis ("Vertical");
+		this._rigid.AddForce(this.transform.forward * vertical * 200 / (this._rigid.velocity.magnitude + 0.1f) , ForceMode.Force);
+	}
+}
---
+++ Assets/Code/FollowingCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingCamera : MonoBehaviour {
+
+	public Transform _lookAt = null;
+	public Transform _followPoint = null;
+	public float _velocity = 1f;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		this.transform.position = Vector3.Lerp (this.transform.position, this._followPoint.position, this._velocity * Time.deltaTime);
+		this.transform.LookAt (this._lookAt);
+	}
+}
